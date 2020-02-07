fada9e0151b301eb694e0e676ad50fa382c3874e
Tomos Fearn <tfearn10@outlook.com>
2020-02-07 16:33:49 +0000

    two dacs working on i2c

--- arduino/wheelchair_linx/linx_dac/linx_dac.ino
+++ arduino/wheelchair_linx/linx_dac/linx_dac.ino
@@ -1,24 +1,57 @@
 #include <Wire.h>
 #include <Adafruit_MCP4725.h>
-#define voltsIn 20
+#define FB_voltsIn 20
+#define RL_voltsIn 20
 
-Adafruit_MCP4725 dac; // constructor
+Adafruit_MCP4725 FB_dac; // constructor
+Adafruit_MCP4725 RL_dac;
+
 
 void setup(void) {
   Serial.begin(9600);
-  dac.begin(0x60); // The I2C Address: Run the I2C Scanner if you're not sure  
+  FB_dac.begin(0x60); // The I2C Address: Run the I2C Scanner if you're not sure  
+  RL_dac.begin(0x61);
   
 }
 
+/*void setForwardBackwardDAC() {
+  dac_expected_output = (5.0/4096.0) * dac_value;
+  dac.setVoltage(dac_value, false);
+  delay(250);
+  adcValueRead = analogRead(voltsIn);
+  voltageRead = (adcValueRead * 5.0 )/ 1024.0;
+}
+
+void setLeftRightDAC() {
+  
+}*/
+
 void loop(void) {
   
-    uint32_t dac_value;
-    int adcValueRead = 0;
-    float voltageRead = 0;
+    uint32_t FB_dac_value = 2250;
+    int FB_adcValueRead = 0;
+    float FB_voltageRead = 0;
+    float FB_dac_expected_output;
     
-    float dac_expected_output;
+    FB_dac_expected_output = (5.0/4096.0) * FB_dac_value;
+    FB_dac.setVoltage(FB_dac_value, false);
+    delay(250);
+    FB_adcValueRead = analogRead(FB_voltsIn);
+    FB_voltageRead = (FB_adcValueRead * 5.0 )/ 1024.0;
+
+
+    uint32_t RL_dac_value = 4000;
+    int RL_adcValueRead = 0;
+    float RL_voltageRead = 0;
+    float RL_dac_expected_output;
     
-   
+    RL_dac_expected_output = (5.0/4096.0) * FB_dac_value;
+    RL_dac.setVoltage(RL_dac_value, false);
+    delay(250);
+    RL_adcValueRead = analogRead(RL_voltsIn);
+    RL_voltageRead = (RL_adcValueRead * 5.0 )/ 1024.0;
+    
+   /*
     for (dac_value = 0; dac_value < 4096; dac_value = dac_value + 15)
     {
       dac_expected_output = (5.0/4096.0) * dac_value;
@@ -38,5 +71,6 @@ void loop(void) {
       
       Serial.print("\tArduino Voltage: ");      
       Serial.println(voltageRead,3);      
-    }    
+    }
+    */
 }
